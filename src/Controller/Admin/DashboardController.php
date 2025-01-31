743ef8cb95fc12590147f67ae74663c9c3ab0c85
Titi7750 <tristanfio05@gmail.com>
2025-01-31 18:13:56 +0100

    [DEV]: Add CRUD in administration

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,6 +8,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\News;
+use App\Entity\User;
+use App\Entity\Contact;
 use App\Entity\Internship;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -29,6 +31,10 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        
+        yield MenuItem::section('Utilisateurs');
+        yield MenuItem::linkToCrud('Les utilisateurs', 'fas fa-users', User::class);
+        yield MenuItem::linkToCrud('Contact', 'fas fa-envelope', Contact::class);
 
         yield MenuItem::section('Actualités/Témoignages');
         yield MenuItem::linkToCrud('Les actualités', 'fas fa-newspaper', News::class);
