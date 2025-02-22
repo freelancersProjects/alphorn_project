7587ccf284b53a0d0e39f3a8930e2044135bf1f9
Titi <tristanfio05@gmail.com>
2025-02-22 10:57:05 +0100

    Add comment for title page

--- src/Controller/InternshipController.php
+++ src/Controller/InternshipController.php
@@ -4,12 +4,20 @@ namespace App\Controller;
 
 use App\Entity\Internship;
 use App\Repository\InternshipRepository;
+use App\Service\TranslationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class InternshipController extends AbstractController
 {
+    private $translationService;
+
+    public function __construct(TranslationService $translationService)
+    {
+        $this->translationService = $translationService;
+    }
+
     #[Route('/stage', name: 'app_internship')]
     public function index(InternshipRepository $internshipRepository): Response
     {
